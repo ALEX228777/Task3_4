64927368ac4465398dacdc844be42f21288c9a3a
ALEX228777 <119002639+ALEX228777@users.noreply.github.com>
2024-03-12 22:18:55 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -24,13 +24,13 @@ namespace Task3_1
             String stringi = new String();
 
             IPrint[] arrayprint = new IPrint[8];
-            arrayprint[0] = new One_dimension<int>(randi,inti); // Можете объяснить в чем ошибки - я совсем это не понимаю.
-            arrayprint[1] = new One_dimension<double>(randi, doublei);
-            arrayprint[2] = new One_dimension<string>(randi, stringi);
+            arrayprint[0] = new One_dimension<int>(randi,inti)// Можете объяснить в чем ошибки - я совсем это не понимаю.
+            arrayprint[1] = new One_dimension<string>(randi, stringi);
+            arrayprint[2] = new One_dimension<double>(randi, doublei);
             arrayprint[3] = new One_dimension<bool>(randi, booli);
             arrayprint[4] = new Two_dimension<int>(randi, inti);
-            arrayprint[5] = new Two_dimension<double>(randi, doublei);
-            arrayprint[6] = new Two_dimension<string>(randi, stringi);
+            arrayprint[5] = new Two_dimension<string>(randi, stringi);
+            arrayprint[6] = new Two_dimension<double>(randi, doublei);
             arrayprint[7] = new Two_dimension<bool>(randi, booli);
 
             foreach (IPrint i in arrayprint)
